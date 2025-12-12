d719578dee749986db2f875cbf5a1a4f46e6f322
Garriga78 <alejandro@tictag.es>
2025-12-12 16:14:54 +0100

    Add Vercel adapter and fix blog route conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react()]
